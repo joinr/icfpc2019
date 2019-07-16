4d9f3d078dec8ae82dfaedcabb19ea450bec6da8
tom <tom@that-place>
2019-07-16 16:49:14 -0500

    Generalized

--- src/icfpc/speed.clj
+++ src/icfpc/speed.clj
@@ -84,7 +84,7 @@
       `[~f ~(get-slot coll idx)])))
   ([coll flds]
    (let [tag (-> coll meta :tag)
-         getter (slot-getter (eval tag))]
+         getter (slot-getter (eval (or tag 'Object)))]
      (slot-binds coll getter  flds))))
 
 (defn key-binds
@@ -93,7 +93,8 @@
     (for [f flds]
       `[~f ~(get-key coll (keyword f))])))
   ([coll flds]
-   (let [getter (-> coll meta :tag eval key-getter)]
+   (let [tag (-> coll meta :tag)
+         getter (key-getter (eval (or tag 'Object)))]
      (key-binds coll getter flds))))
 
 (defn ensure-distinct [& ks]
@@ -103,66 +104,177 @@
     (when (seq shared)
       (throw (ex-info "duplicate fields or keys detected, check your destructuring form!"
                       {:shared shared })))))
-    
-(defmacro with-fields
+
+(defn as-binds
+  ([fields coll]
+   (let [tag    (or (some-> coll meta :tag name symbol)
+                    (cond (map? coll)   'clojure.lang.IPersistentMap
+                          (vector? coll) 'clojure.lang.IPersistentVector
+                          (set? coll)    'clojure.lang.IPersistentSet
+                          :else          'Object))
+         tagged (with-meta (gensym "coll") {:tag tag})]
+     (as-binds tagged fields coll)))
+  ([tagged fields coll]
+   (let [tag (-> tagged meta :tag)]
+     (cond  (= tag 'Object)
+            (do (assert (not (:fields fields)) "cannot use :fields key with untyped object!")
+                `[~fields ~coll])
+            (map? fields) ;;{:keys flds :as something}      
+            (let [flds   (get fields :fields)
+                  ks     (get fields :keys)
+                  _      (ensure-distinct flds ks)
+                  tgt    (when-let [res (get fields :as)]
+                           (if (-> res meta :tag)
+                             res
+                             (with-meta res {:tag tag})))]
+              `[~tagged ~coll
+                ~@(into (if tgt `[~tgt ~coll] [])
+                        (concat (field-binds tagged flds)
+                                (key-binds   tagged ks)))])
+            (vector? fields)
+            (do (ensure-distinct fields)
+                (if (= tagged coll)
+                  `[~@(slot-binds tagged fields)]
+                  `[~tagged ~coll
+                    ~@(slot-binds tagged fields)]))
+            :else (throw
+                   (ex-info "invalid field spec"
+                            {:fields fields :expected [:or :map :vector]}))))))
+  
+#_(defmacro with-fields
   [fields coll & body]
-  (let [[_ flds coll & rest] &form
-        tag    (or (some-> coll meta :tag name symbol)
-                   (cond (map? coll)   'clojure.lang.IPersistentMap
-                         (vector? coll) 'clojure.lang.IPersistentVector
-                         (set? coll)    'clojure.lang.IPersistentSet
-                         :else          'Object))
-        tagged (with-meta (gensym "coll") {:tag tag})]
-    (cond  (= tag 'Object)
-           (do (assert (not (:fields flds)) "cannot use :fields key with untyped object!")
-               `(let [~fields ~coll]
-                  ~@body))
-           (map? fields) ;;{:keys flds :as something}         
-           (let [flds   (get fields :fields)
-                 ks     (get fields :keys)
-                 _      (ensure-distinct flds ks)
-                 tgt    (when-let [res (get fields :as)]
-                          (if (-> res meta :tag)
-                            res
-                            (with-meta res {:tag tag})))]
-              `(let [~tagged ~coll
-                     ~@(into (if tgt `[~tgt ~coll] [])
-                             (concat (field-binds tagged flds)
-                                     (key-binds   tagged ks)))]
-                 ~@body))          
-           (vector? fields)
-           (do _ (ensure-distinct flds)
-               `(let [~tagged ~coll
-                      ~@(slot-binds tagged flds)]
-                  ~@body))
-          :else (throw
-                 (ex-info "invalid field spec"
-                          {:fields fields :expected [:or :map :vector]})))))
+  (let [[_ fields coll & rest] &form]
+    `(let [~@(as-binds fields coll)]
+       ~@body)))
 
-(defmacro with-slots [bindings & body]
-  (let [_ (assert (vector? bindings) "slot bindings must be a vector!")
-        [_ bindings & body] &form
-        n  (count bindings)]
-    (cond (zero? n) `~(first body)
-          (and (even? n)(pos? n)
-               (symbol? (first bindings)))
-          
-          (let [[l r] (take 2 bindings)]
-            `(let [~l ~r]
-               (with-slots ~(vec (drop 2 bindings)) ~@body)))
-          
-          (= n 2)  (let [[l r]  bindings]
-                     
-                       `(with-fields ~l ~r ~@body))
-          (even? n)
-          (let [[l r] (vec (take 2 bindings))]                         
-            `(with-fields ~l ~r
-               (with-slots ~(vec (drop 2 bindings)) ~@body)))
-          :else (throw (ex-info "bindings must have an even number of entries!" {})))))
+(defn scrape-symbols [l]
+  (cond (symbol? l) l
+        (map? l)    (concat (:fields l)
+                            (:keys l))
+        (vector? l) l
+        :else nil))
+        
+(defn unify-binds [bindings]
+  (let [n       (count bindings)
+        _  (assert (and (vector? bindings)
+                        (pos? n)
+                        (even? n)) "slot bindings must be a vector with an even number of entries!")
+        tags (atom {})       
+        get-tag (fn [x]
+                  (let [k (name x)]
+                    (if-let [t (-> x meta :tag)]
+                      (if-not (@tags k)
+                        (do (swap! tags assoc k t)
+                            t)
+                        (do (when (not= t (@tags k))
+                              (println [:with-slots.warning/changed-tags
+                                        :symbol x :tag t :previously (@tags k)]))
+                            t))
+                      (@tags k))))
+        tag (fn [x]
+              (if-let [t (get-tag x)]
+                (with-meta x {:tag t})
+                x))
+        tag-bind (fn [[l r]]
+                   (let [scrape? (or (map? l) (vector? l))
+                         xs (when scrape? (mapv tag (scrape-symbols l)))]
+                     [l (if (symbol? r) (tag r) r)]))
+        n (atom 0)
+        res  (apply concat (for [b (partition 2 bindings)]
+                             (let [[l r] (tag-bind b)
+                                   re-use? (and (pos? @n)
+                                                (symbol? r))
+                                   _ (swap! n inc)]
+                               (if re-use?
+                                 (as-binds r l r)
+                                 (as-binds l r)))))]
+    res))
 
+(defmacro with-slots
+  "Allows for efficient, type-based destructuring similar to the
+  idiomatic destructuring forms of Clojure, with some limitations.
+  Bindings are presented as the typical vector, with an even number of
+  entries, where the preceding odd binding establishes binds for the
+  even successor.  Unlike typical forms, bindings leverage
+  type-hinting information - both on the left hand side and the right
+  hand side - to establish efficient operations beyond the generic
+  destructuring forms established with maps and vectors, e.g. get and
+  nth.
 
+  Callers may use {:fields [a b ^clojure.lang.Counted c] }, along with
+  a type-hinted rhs, to denote establishing bindings for a, b, c, by
+  invoking like-named direct, type-hinted field applications on the
+  rhs, ala (.a ^some-type rhs).
+
+  Any binding var hinted on the LHS will propogate its hint throughout
+  later bindings.  This allows an expressive form of efficient
+  destructuring for the consenting adult, which allows idiomatic
+  expressivity without the accompanying significant loss of
+  performance.
+
+  map destructuring for {:keys [...]} follows that of :fields, except
+  the bindings are established via either a (.valAt ..) or (.get ..)
+  or (get ...) depending on the presented type, get being the fallback.
+  This allows usage with types supporting the java.util.Map interface.
+  Literal maps are automatically inferred with efficient getters.
+
+  Vector or indexed destructuring is similarly supported,
+  [^some-type x y] ^clojure.lang.Indexed coll will invoke efficient
+  .nth indexing operations rather than the slower, more general nth.
+  Depending on the presented type, either .nth, .get, or nth will be
+  used, allowing operation with structures supporting the
+  java.util.List interface.  Literal vectors are automatically
+  inferred with efficient getters.  The & rest notation is currently
+  NOT supported...
+
+  The remaining rules act identically to let semantics.  If a symbol
+  is bound to the LHS, then the binding is passed through
+  untouched (including hints).
+
+  with-slots tries to scan the input bindings to find
+  discrepancies (such as duplicate binds), and to re-use existing
+  hinted information for binds.  In the case that the user decides to
+  re-hint a RHS var that has already been hinted a-priori, with-slots
+  will allow the hint for that binding, but revert to prior hinting
+  unless the user continues to specify new hints.  This seems rare in
+  practice.
+
+  It's common to import the symbols for the
+  [clojure.lang Counted Indexed] interfaces when using with-slots.
+
+  An example:
+  
+  (with-slots
+    [{:fields [^Counted path
+               ^Indexed position]} ^botmove (->botmove [] [1 2])
+     {:keys [a b] :fields [hashCode]}    {:a 2 :b 3}
+     [x y]          position         
+     path-length   (.count path)]
+    [hashCode (+ x y)])
+  "
+  [bindings & body]
+  (let [[_ bindings & body] &form]
+    `(let [~@(unify-binds bindings)]
+       ~@body)))
 
 (comment ;testing
+
+  (defrecord botmove [path position])
+ 
+  (binding [*print-meta* true]
+    (pprint
+     (macroexpand-all
+      '(with-slots [{:fields [^clojure.lang.Counted path  ^clojure.lang.Indexed position]} ^botmove move                                  
+                    [x y]         pos         
+                    path-length   (.count path)]
+         2))))
+  
+  (with-slots [{:fields [^clojure.lang.Counted path
+                         ^clojure.lang.Indexed position]} ^botmove (->botmove [] [1 2])
+               {:keys [a b] :fields [hashCode]}    {:a 2 :b 3}
+                [x y]          position         
+               path-length   (.count path)]
+    [hashCode (+ x y)])
   
 #_(binding [*print-meta* true]
   (pprint
